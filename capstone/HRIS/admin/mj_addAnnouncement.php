3e7a0ace939d44d0827f1169c4b68b2c4013bdb7
LinTech <hernandezmarkjulius24@gmail.com>
2023-06-25 18:29:26 +0800

    add aanouncement function

--- capstone/HRIS/admin/mj_addAnnouncement.php
+++ capstone/HRIS/admin/mj_addAnnouncement.php
@@ -17,28 +17,19 @@ ob_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
-        integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/scroller/2.0.5/js/dataTables.scroller.min.js"></script>
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.datatables.net/scroller/2.0.5/css/scroller.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/scroller/2.0.5/css/scroller.dataTables.min.css">
 
 
     <link rel="stylesheet" href="https://cdn.datatables.net/searchpanes/2.1.2/css/searchPanes.dataTables.min.css">
@@ -46,7 +37,7 @@ ob_start();
 
 
     <link rel="stylesheet" href="../login/css/navigation.css">
-    <link rel="stylesheet" href="annoucement.css">
+    <link rel="stylesheet" href="edit.css">
 
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
@@ -68,8 +59,7 @@ ob_start();
                     <a href="../login/mj_mainPage.php?idNumber=<?php echo $idNumber; ?>" class="navLink">Home</a>
                 </li>
                 <li class="navItem">
-                    <a href="../attendance/mj_attendance.php?idNumber=<?php echo $idNumber; ?>"
-                        class="navLink">Attendance</a>
+                    <a href="../attendance/mj_attendance.php?idNumber=<?php echo $idNumber; ?>" class="navLink">Attendance</a>
                 </li>
                 <li class="navItem">
                     <a href="../leave/mj_leave.php?idNumber=<?php echo $idNumber; ?>" class="navLink">Leave</a>
@@ -91,17 +81,12 @@ ob_start();
 
     <!-- header title -->
     <div class="adminHeaderContainer">
-        <span class="back" onclick="window.location.href='mj_adminPanel.php'"><i
-                class="fa-solid fa-backward"></i></span>
+        <span class="back" onclick="window.location.href='mj_adminPanel.php'"><i class="fa-solid fa-backward"></i></span>
         <p class="title">Annoucement Software</p>
     </div>
 
 
     <main class="containers">
-        <div class="addContainer">
-            <button class="add" onclick="window.location.href='mj_addAnnouncement.php'"><i
-                    class="fa-solid fa-plus"></i></button>
-        </div>
         <div class="tableContainer">
             <table id="userTable" class="table table-info table-striped caption-top table-hover" style="width:100%;">
                 <thead>
@@ -123,7 +108,6 @@ ob_start();
                     $i = 1;
                     if ($query && $query->num_rows > 0) {
                         while ($row = $query->fetch_assoc()) {
-                            $announcementId = $row['announcementId'];
                             $scheduledDate = $row['scheduledDate'];
                             $scheduledTime = $row['scheduledTime'];
                             $newTime = date('h:i:s', strtotime($scheduledTime));
@@ -133,16 +117,13 @@ ob_start();
 
                             echo "
                                 <tr>
-                                <td  class='align-middle'>" . $i++ . "</td>
-                                <td  class='align-middle'>$scheduledDate</td>
-                                <td  class='align-middle'>$newTime</td>
-                                <td  class='align-middle'>$purpose</td>
-                                <td  class='align-middle'>$venue</td>
-                                <td  class='align-middle'>$type</td>
-                                <td class='align-middle' style='text-align:center;'>
-                                <i class='fa-solid fa-pen-to-square' id='button' onclick=\"window.location.href='mj_announcementAjax.php?announcementId=" . $announcementId . "'\"></i>
-                              </td>
-                              
+                                <td   td  class='align-middle'>" . $i++ . "</td>
+                                <td   td  class='align-middle'>$scheduledDate</td>
+                                <td   td  class='align-middle'>$newTime</td>
+                                <td   td  class='align-middle'>$purpose</td>
+                                <td   td  class='align-middle'>$venue</td>
+                                <td   td  class='align-middle'>$type</td>
+                                <td td class='align-middle'><i class='fa-solid fa-pen-to-square' id='button'></i></td>
                                 <tr>
                             
                             ";
@@ -156,6 +137,57 @@ ob_start();
         </div>
     </main>
 
+    <?php
+
+    if (isset($_POST['apply'])) {
+        $scheduledDate = $_POST['scheduledDate'];
+        $scheduledTime = $_POST['scheduledTime'];
+        $purpose = $_POST['purpose'];
+        $venue = $_POST['venue'];
+        $type = $_POST['type'] == "Meeting" ? 0 : 1;
+
+        $sql = "INSERT INTO hr_annoucement (scheduledDate, scheduledTime, purpose, venue, type) VALUES ('$scheduledDate', '$scheduledTime', '$purpose', '$venue', '$type')";
+
+        $updateQuery = $connectionString->query($sql);
+        if ($updateQuery && $updateQuery) {
+            header("location: mj_announcement.php");
+        } else {
+            header("location: mj_announcement.php");
+        }
+    }
+
+
+
+    echo '<div id="myModal" class="modal">
+        <div class="modal-content">
+            <h1 class="text">Annoucement</h1>
+    <div class="data">
+        <form method="POST" id="announcement"> </form>
+    <input type="date" name="scheduledDate" id="" form="announcement" class="inputs">
+    <input type="text" name="scheduledTime" id="time" form="announcement" class="inputs" placeholder="Scheduled Time">
+    <input type="text" name="purpose" id="" form="announcement"  class="inputs purpose" placeholder="Purpose">
+    <input type="text" name="venue" id="" form="announcement" class="inputs" placeholder="Venue">
+    <select name="type" id="" class="inputs" form="announcement">
+        <option value="" selected disabled hidden>
+        Type
+        </option>
+    <option value="0">Meeting</option>
+    <option value="1">General</option>
+    </select>
+    </div>
+    <div class="buttonContainer">
+        <input type="submit" value="Apply" id="go" form="announcement" name="apply">
+
+        <input type="button" value="Cancel" id="close">
+    </div>
+
+    </div>
+    </div>';
+
+    ?>
+
+
+
 
 
 
@@ -163,32 +195,10 @@ ob_start();
 
 </html>
 <script>
-// $(document).ready(function() {
-//     $("i#button").click(function() {
-//         var value = $(this).attr("data-value");
-//         console.log(value);
+    $(document).ready(function() {
+        $("#close").click(function() {
+            window.location.href = "mj_announcement.php";
+        })
 
-//         $.ajax({
-//             url: 'mj_announcementAjax.php',
-//             method: 'POST',
-//             data: {
-//                 value: value
-//             },
-//             success: function(response) {
-
-//                 /*            $("#myModal").remove(); */
-//                 $("#result").html(response);
-//                 $("#myModal").show();
-//             }
-//         })
-//     });
-
-
-//     $(document).on("click", "#close", function() {
-//         $("#myModal").hide(); // Close the modal
-//     });
-
-
-
-// });
+    });
 </script>
